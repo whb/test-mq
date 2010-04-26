b592621f3c4e8851ecbb4b2cc6ec24a4649f23ac
Wu Haibo <wuhaibo@ceopen.cn>
2010-04-26 13:24:50 +0800

    Add batch file usage help.

--- bin/client.bat
+++ bin/client.bat
@@ -1,17 +1,24 @@
 @echo off
-@rem Usage client [activemq|hornetq] <spring_file.xml>
+@rem Usage: client [activemq|hornetq] <spring_file.xml>
 
+if "%1"=="activemq" goto :MAIN
+if "%1"=="hornetq"  goto :MAIN
+
+:USAGE
+echo ====================================================
+echo Usage: %0 activemq spring_file.xml 
+echo        OR
+echo Usage: %0 hornetq spring_file.xml
+echo =====================================================
+goto :EOF
+
+:MAIN
 set APP_HOME=%~dp0..
-
-if activemq==%1 (
-	set CONFIG_DIR=config\client\activemq
-) else (
-	set CONFIG_DIR=config\client\hornetq
-)
-
+set CONFIG_DIR=config\client\%1
 set FILE_DIR=config\client
 set FILE=%2
 set CP=%CONFIG_DIR%;%FILE_DIR%;%APP_HOME%\lib\*;%APP_HOME%\target\dependency\*;%APP_HOME%\target\classes;
-
 echo java %JAVA_OPTS% -cp %CP% com.sitechasia.mq.bootstrap.SpringContainer "%FILE_DIR%\%FILE%"
 java %JAVA_OPTS% -cp %CP% com.sitechasia.mq.bootstrap.SpringContainer "%FILE_DIR%\%FILE%"
+
+:EOF
--- bin/server.bat
+++ bin/server.bat
@@ -1,20 +1,30 @@
 @echo off
-@rem Usage server [activemq|hornetq]
+@rem Usage: server [activemq|hornetq]
 
+if "%1"=="activemq" goto :MAIN
+if "%1"=="hornetq"  goto :MAIN
+
+:USAGE
+echo ====================================================
+echo Usage: %0 activemq 
+echo        OR
+echo Usage: %0 hornetq
+echo =====================================================
+goto :EOF
+
+:MAIN
 set APP_HOME=%~dp0..
 
 if activemq==%1 (
-	set FILE_DIR=config\server\activemq
-	set FILE=boot.xml
-	set SERVER_OPTS=-Dactivemq.base=%APP_HOME%\target\activemq
+	set MQ_OPTS=-Dactivemq.base=%APP_HOME%\target\activemq
 ) else (
-	set FILE_DIR=config\server\hornetq
-	set FILE=boot.xml
-	set SERVER_OPTS=-Dhornetq.data.dir=%APP_HOME%\target\hornetq\data
+	set MQ_OPTS=-Dhornetq.data.dir=%APP_HOME%\target\hornetq\data
 )
-
+set FILE_DIR=config\server\%1
+set FILE=boot.xml
 set CP=%FILE_DIR%;%APP_HOME%\lib\*;%APP_HOME%\target\dependency\*;%APP_HOME%\target\classes;
 set JAVA_OPTS=-DwaitForShutdown=true
 
-echo java %JAVA_OPTS% %SERVER_OPTS% -cp %CP% com.sitechasia.mq.bootstrap.SpringContainer "%FILE_DIR%\%FILE%"
-java %JAVA_OPTS% %SERVER_OPTS% -cp %CP% com.sitechasia.mq.bootstrap.SpringContainer "%FILE_DIR%\%FILE%"
+java %JAVA_OPTS% %MQ_OPTS% -cp %CP% com.sitechasia.mq.bootstrap.SpringContainer "%FILE_DIR%\%FILE%"
+
+:EOF
